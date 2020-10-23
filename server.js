7d81c08ee59463b81deca87fed6a0c13e8ceec01
David Morgan <ball41@gmail.com>
2020-10-24 10:38:45 +1100

    Update package.json

--- server.js
+++ server.js
@@ -48,10 +48,10 @@ app.use('/drivers', driversRouter);
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('../client/build'));
+    app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../client', '../client/build', '../client/public/index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'client/build', 'client/public/index.html'));
     });
 
     /* app.use(express.static(path.join(__dirname, '../build')));
