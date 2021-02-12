2df60913bce8e6f407aeb2512415c8d2831c2c6e
seren1ty <ball41@gmail.com>
2021-02-12 16:09:34 +1100

    Styling for Admin data box edit mode

--- client/src/components/common/admin-data-boxes.tsx
+++ client/src/components/common/admin-data-boxes.tsx
@@ -12,12 +12,55 @@ type AdminDataProps = {
 
 const AdminDataBoxes = (props: AdminDataProps) => {
 
-    const [hoveredId, setHoveredId] = useState("");
+    const [hoveredId, setHoveredId] = useState('');
+    const [showConfirm, setShowConfirm] = useState(false);
+    const [showEdit, setShowEdit] = useState(false);
+    const [newItemName, setNewItemName] = useState('');
 
     const onHoverBox = (itemId: string) => {
+        if (showConfirm || showEdit)
+            return;
+
         console.log(itemId);
 
         setHoveredId(itemId);
+    }
+
+    const onLeaveBox = () => {
+        if (showConfirm || showEdit)
+            return;
+
+        setHoveredId("");
+    }
+
+    const onClickDelete = () => {
+        setShowConfirm(true);
+    }
+
+    const onClickDeleteConfirm = () => {
+
+    }
+
+    const onClickDeleteCancel = () => {
+        setShowConfirm(false);
+    }
+
+    const onClickEdit = (dataItem: AdminBoxData) => {
+        setNewItemName(dataItem.name);
+
+        setShowEdit(true);
+    }
+
+    const onChangeNewItemName = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setNewItemName(event.target.value);
+    }
+
+    const onClickEditConfirm = () => {
+
+    }
+
+    const onClickEditCancel = () => {
+        setShowEdit(false);
     }
 
     return (
@@ -28,15 +71,47 @@ const AdminDataBoxes = (props: AdminDataProps) => {
                     <div className="data-box" key={dataItem._id}
                         onClick={() => onHoverBox(dataItem._id)}
                         onMouseEnter={() => onHoverBox(dataItem._id)}
-                        onMouseLeave={() => setHoveredId("")}>
+                        onMouseLeave={onLeaveBox}>
                     {
-                        hoveredId === dataItem._id &&
-                        <div className="data-box-edit">Edit</div>
-                    }
+                        (!showEdit || (showEdit && hoveredId !== dataItem._id)) &&
                         <Truncator id={"track_" + dataItem._id} value={dataItem.name} max={19}/>
+                    }
                     {
                         hoveredId === dataItem._id &&
-                        <div className="data-box-delete">Delete</div>
+                        <div>
+                        {
+                            !showEdit &&
+                            <div>
+                            {
+                                !showConfirm &&
+                                <div>
+                                    <div className="data-box-edit" onClick={() => onClickEdit(dataItem)}>Edit</div>
+                                    <div className="data-box-delete" onClick={onClickDelete}>Delete</div>
+                                </div>
+                            }
+                            {
+                                showConfirm &&
+                                <div>
+                                    <div className="data-box-delete-confirm" onClick={onClickDeleteConfirm}>Delete</div>
+                                    <div className="data-box-delete-cancel" onClick={onClickDeleteCancel}>Cancel</div>
+                                </div>
+                            }
+                            </div>
+                        }
+
+                        {
+                            showEdit &&
+                            <div>
+                                <input className="data-box-edit-input"
+                                    type="text"
+                                    value={newItemName}
+                                    onChange={onChangeNewItemName}
+                                />
+                                <div className="data-box-edit-confirm" onClick={onClickEditConfirm}>Update</div>
+                                <div className="data-box-edit-cancel" onClick={onClickEditCancel}>Cancel</div>
+                            </div>
+                        }
+                        </div>
                     }
                     </div>
                 )
