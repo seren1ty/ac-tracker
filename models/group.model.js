82fcee6011858e2717fb37a4d7b1b4042a797c32
seren1ty <ball41@gmail.com>
2021-03-10 16:42:04 +1100

    Create Group schema

---
+++ models/group.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const groupSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 2
+    },
+    description: {
+        type: String
+    },
+    adminId: {
+        type: String
+    }
+}, {
+    timestamps: true
+});
+
+const Group = mongoose.model('Group', groupSchema);
+
+module.exports = Group;
